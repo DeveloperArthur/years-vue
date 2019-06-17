658f9142fdcc0968c02ce8aa1ee04297c51e1102
Arthur Santos Almeida <42553658+DeveloperArthur@users.noreply.github.com>
2019-06-17 15:21:56 -0300

    Add files via upload

---
+++ src/App.vue
@@ -0,0 +1,23 @@
+<template>
+  <div id="app">
+    <app-years></app-years>    
+  </div>
+</template>
+
+<script>
+import years from './components/yeas.vue';
+
+export default {
+  data () {
+    return {
+      idade: 0
+    }
+  },
+  components: {
+    AppYears: years
+  }
+}
+</script>
+
+<style>
+</style>
---
+++ src/components/yeas.vue
@@ -0,0 +1,21 @@
+<template>
+  <div>
+    <input v-model.number="idade" type="number">
+    <button type="submit" @click="whaiYear(idade)">Enviar</button>
+  </div>
+</template>
+
+<script>
+export default {
+  methods: {
+    whaiYear(idade) {
+      if (idade < 18) {
+        alert("Você é menor de idade");
+      } else {
+        alert("Você é maior de idade");
+      }
+    }
+  }
+};
+</script>
+
